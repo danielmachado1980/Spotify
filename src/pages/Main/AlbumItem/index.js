4b5215f5273096aede8ce038102eab5f74e1c2ac
Daniel Machado <daniel.g_amachado@hotmail.com>
2019-01-24 05:11:25 -0200

    Layout da playlist

--- src/pages/Main/AlbumItem/index.js
+++ src/pages/Main/AlbumItem/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+ View, Text, Image, TouchableOpacity 
+} from 'react-native';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
 
 const AlbumItem = ({ album }) => (
-  <View style={styles.container}>
-    <Text>{album.title}</Text>
-  </View>
+  <TouchableOpacity onPress={() => {}} style={styles.container}>
+    <Image style={styles.thumbnail} source={{ uri: album.thumbnail }} />
+    <View style={styles.info}>
+      <Text style={styles.title}>{album.title}</Text>
+      <Text style={styles.author}>{album.author}</Text>
+    </View>
+    <Icon name="more-horiz" size={24} style={styles.more} />
+  </TouchableOpacity>
 );
 
 export default AlbumItem;
--- src/pages/Main/AlbumItem/styles.js
+++ src/pages/Main/AlbumItem/styles.js
@@ -1,10 +1,31 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '~/styles';
+import { colors, metrics } from '~/styles';
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: metrics.basePadding,
+    marginHorizontal: metrics.basePadding,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  thumbnail: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+  },
+  info: {
+    marginLeft: metrics.baseMargin,
     flex: 1,
-    backgroundColor: colors.white,
+  },
+  title: {
+    fontSize: 15,
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+  author: {
+    fontSize: 14,
+    color: colors.dark,
+    marginTop: 3,
   },
 });
 
